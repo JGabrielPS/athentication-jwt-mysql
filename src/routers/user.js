8020b747af1bb1d6f67e918352138878a4e23213
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2020-01-06 13:06:51 -0300

    Agregadas las primeras rutas: buscar todos los usuarios y registrar usuario

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const User = require('../models/User')
+const express = require("express");
+const User = require("../models/User");
 
-const router = express.Router()
+const router = express.Router();
 
-// router.post('/users', async (res, req) => {
-//   try{
-//     const User
-//   }catch(err) {
-//     res.status(400).send(err)
-//   }
-// })
+router.get("/users", (req, res) => {
+  User.getAllUsers()
+    .then(user => res.status(200).json(user))
+    .catch(err => console.log(err));
+});
+
+router.post("/register", (req, res) => {
+  const { userName, email, password } = req.body;
+  User.createUser(userName, email, password)
+    .then(user => {
+      res.status(200).json({ user, msg: "Account created successfully" });
+    })
+    .catch(err => console.log(err));
+});
+
+module.exports = router;
