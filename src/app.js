8417bd3958221a503648ceabeb6a7e15c89e19b1
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2020-01-06 13:06:13 -0300

    Descomentado el app.use del ruteador de usuario

--- src/app.js
+++ src/app.js
@@ -5,8 +5,16 @@ require('./db/db')
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+}) 
+
 app.use(express.json())
-//app.use(userRouter)
+app.use(userRouter)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
